97b27100e214b9122b0f08d570724a55a4581c0a
sspboy <sspboy@163.com>
2022-10-28 17:38:09 +0800

    'task'

---
+++ Task/script_model/定时任务.js
@@ -0,0 +1,17 @@
+// 定时任务1
+// var num = 1
+// const heart_beat = setInterval(()=>{
+    
+//     console.log('主线程!第' + num + '次');
+
+//     num = num + 1;
+
+// },5000);
+
+// 定时任务二
+// const heart = setInterval(()=>{
+    
+//     console.log('子线程，请求任务!');
+
+
+// },1000);
