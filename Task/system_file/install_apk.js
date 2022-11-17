edd01c74f10260da674515a98a855dbca603bd65
sspboy <sspboy@163.com>
2022-11-17 17:49:08 +0800

    '1117'

--- Task/system_file/install_apk.js
+++ Task/system_file/install_apk.js
@@ -1,11 +1,27 @@
 // 安装应用
 
-
 // 打开应用市场
+var name = app.getPackageName('华为应用市场');
+console.log(name);
+app.launch(name)
 // 搜索应用名称
+sleep(6000)
+className("LinearLayout").findOne(2000).click() // 点击搜索框
+sleep(2000)
+
+className("EditText").findOne(2000).setText('微信') // 输入框输入应用关键字
 // 点击安装
+if(text('微信').exists()){
+    text('微信').findOnce(1).click()
+}
 // 获取应用名称
+
 // 返回是否成功
+
+
+
+
+
 
 // 登录方法
 
@@ -25,7 +41,8 @@
  * 4、选择第一行图片
  * 5、判断扫码执行是否完成
  * */
-
+ var name = app.getPackageName('微信');
+ app.launch(name)
 
 
 
